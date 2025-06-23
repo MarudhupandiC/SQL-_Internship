f3b1f3e0ff12ee392a933da5cfbdd9e4f1d96703
MarudhupandiC <maruchelliah007@gmail.com>
2025-06-23 15:03:33 +0530

    Add files via upload

---
+++ task 1 - 23.06.2025.sql
@@ -0,0 +1,13 @@
+
+-- use sql_intern
+-- show tables
+
+-- create table library(book_id int auto_increment primary key, book_name varchar(30), 
+-- author_id int, category_id int, unique key(author_id))
+
+-- desc library
+
+-- create table members(member_id int auto_increment primary key, member_name varchar(30), book_id int,
+-- foreign key(book_id)references library(book_id))
+
+-- desc members
